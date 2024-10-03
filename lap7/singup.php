59208bb5992044a3a7c7fca214665a5c1a6e457d
Narathorn6711518 <67111518@dpu.ac.th>
2024-10-03 11:53:34 +0700

    singup

---
+++ lap7/singup.php
@@ -0,0 +1,32 @@
+<?php
+require 'db.php'; // คือใช้ไว้ดึงข้อมูลจากข้อมูลที่ต้องการดึงมา
+
+if (isset($_POST['signup'])) { //isset คือเช็คว่ามีความสั่ง post มั้ย
+    $username = $_POST['username'];     
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // เข้ารหัสรหัสผ่าน
+try {
+    $sql = "SELECT  studentID  
+            FROM student 
+            WHERE studentID=?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows > 0) { 
+            $sql = "INSERT INTO users (username, password) VALUES (?, ?)"; //เรียงตามลำดับที่สร้าง คืออะไรที่มาจากข้างนอกไว้ใจไม่ได้ให้ใส่ ?
+            $stmt = $conn->prepare($sql); 
+            $stmt->bind_param("ss", $username, $password); //เรียกใช้ stmt ส่งข้อมูล เป็นไปตามข้อมูลที่ต้องการ
+            $stmt->execute();  // 
+            echo 'Success';  
+    }
+  else {
+    echo 'Student ID not found.';
+    }
+}
+catch (Exception $e) {
+    echo $e->getMessage();
+    }    
+}    
+        
+?>
